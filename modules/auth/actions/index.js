d55f3ec8d8b6f6a60f9df714c1aa4e3f63d1ac95
Aaddityaaa <adityau5090@gmail.com>
2026-01-07 23:53:37 +0530

    fix prisma error and onboarding user in db

---
+++ modules/auth/actions/index.js
@@ -0,0 +1,52 @@
+"use server"
+
+import { db } from "@/lib/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { success } from "zod";
+
+export const onBoardUser = async () => {
+    try {
+        const user = await currentUser();
+        // console.log(user.emailAddresses[0].emailAddress);
+
+        if(!user){
+            return {
+                success: false,
+                error: "No authenticated user found"
+            }
+        }
+
+        const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+
+        const newUser = await db.user.upsert({
+            where:{
+                clerkId: id
+            },
+            update:{
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses?.[0]?.emailAddress || "",
+            },
+            create:{
+                clerkId: id,
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses?.[0]?.emailAddress || "",
+            }
+        })
+
+        return {
+            success: true,
+            message: "User onBoarded successfully",
+            data: newUser
+        }
+    } catch (error) {
+        console.error("❌ Error on boarding user : ", error);
+        return {
+            success: false,
+            error: `Failed to onBoard user : ${error}`
+        }
+    }
+}
